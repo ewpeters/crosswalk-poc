b0738ec133deff5f31437b6429e023eeb8d62570
Erik Petersen <erik.w.petersen86@gmail.com>
2017-10-06 11:52:59 -0700

    Tweaks for Android 19
    re-embed crosswalk webview; changed targetSdk versions; removed permission prompting code

--- xWalkEmbed/src/main/java/com/example/xwalkembed/MainActivity.java
+++ xWalkEmbed/src/main/java/com/example/xwalkembed/MainActivity.java
@@ -16,20 +16,10 @@ import java.util.List;
 
 public class MainActivity extends Activity {
   private XWalkView mXWalkView;
-  private static final String[] permissions = {Manifest.permission.RECORD_AUDIO, android.Manifest.permission.CAMERA};
-  private List<String> requiredPermissions = new ArrayList<>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    for (String permission : permissions) {
-        requiredPermissions.add(permission);
-    }
-    if (requiredPermissions.size() != 0) {
-      String[] runtimePermissions = new String[requiredPermissions.size()];
-      runtimePermissions = requiredPermissions.toArray(runtimePermissions);
-      requestPermissions(runtimePermissions, 100);
-    }
 
     XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
     setContentView(R.layout.activity_main);
